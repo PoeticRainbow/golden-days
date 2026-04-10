18f929dcdbb93dfbb35b7382042436b3913fc218
PoeticRainbow <jarrett.bayer@outlook.com>
2026-04-10 03:07:10 -0400

    add sodium support for fog (requires sodium core shader support)

---
+++ golden-days-base/shaders_core_21_11/assets/sodium/shaders/include/fog.glsl
@@ -0,0 +1,24 @@
+#import <golden_days:include/general.glsl>
+
+const int FOG_SHAPE_SPHERICAL = 0;
+const int FOG_SHAPE_CYLINDRICAL = 1;
+
+float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
+    return goldenDaysLinearFogFactor(vertexDistance, fogEnd);
+}
+
+float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
+    return goldenDaysLinearFogFactor(sphericalVertexDistance, min(environmentalEnd, renderDistanceEnd));
+}
+
+vec4 _linearFog(vec4 fragColor, vec2 fragDistance, vec4 fogColor, vec2 environmentFog, vec2 renderFog, float fadeFactor) {
+#ifdef USE_FOG
+    return goldenDaysApplyFog(fragColor, fragDistance.x, renderFog.y, environmentFog.y, fogColor);
+#else
+    return fragColor;
+#endif
+}
+
+vec2 getFragDistance(vec3 position) {
+    return vec2(length(position), length(position));
+}
